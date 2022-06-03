302ab3333a55bb74050168c0cbe207d21a6b287d
ehsan <ehsansaraee6570@yahoo.com>
2022-06-03 21:22:14 +0430

    implementing search by location logic

--- components/SearchFilters.jsx
+++ components/SearchFilters.jsx
@@ -1,13 +1,36 @@
+// files & components
+import { baseUrl, fetchApi } from '@/utils/fetchApi';
 import { filterData, getFilterValues } from '@/utils/filterData';
-import { Box, Flex, Select } from '@chakra-ui/react';
+import noresult from '@/assets/images/noresult.svg';
+// packages
+import {
+   Box,
+   Button,
+   Flex,
+   Icon,
+   Input,
+   Select,
+   Spinner,
+   Text,
+} from '@chakra-ui/react';
 import { useRouter } from 'next/router';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { MdCancel } from 'react-icons/md';
+import Image from 'next/image';
 
 const SearchFilters = () => {
-   const [filters, setFilters] = useState(filterData);
+   // states
+   const [filters] = useState(filterData);
+   const [searchTerm, setSearchTerm] = useState('');
+   const [locationData, setLocationData] = useState();
+   const [showLocations, setShowLocations] = useState(false);
+   const [loading, setLoading] = useState(false);
+
+   // router
    const router = useRouter();
    const { query, pathname } = router;
 
+   // fetch data
    const searchProperties = (filterValues) => {
       const values = getFilterValues(filterValues);
 
@@ -18,6 +41,29 @@ const SearchFilters = () => {
       }
 
       router.push({ pathname, query });
+   };
+
+   // fetch locations
+   useEffect(() => {
+      if (searchTerm) {
+         const fetchData = async () => {
+            setLoading(true);
+            const data = await fetchApi(
+               `${baseUrl}/auto-complete?query=${searchTerm}`
+            );
+            setLoading(false);
+            setLocationData(data?.hits);
+         };
+
+         fetchData();
+      }
+   }, [searchTerm]);
+
+   // clear search
+   const handleLocations = (name) => {
+      searchProperties({ locationExternalIDs: location.externalID });
+      setShowLocations(false);
+      setSearchTerm(name);
    };
 
    return (
@@ -42,6 +88,70 @@ const SearchFilters = () => {
                </Select>
             </Box>
          ))}
+         <Flex flexDir="column">
+            <Button
+               onClick={() => setShowLocations(!showLocations)}
+               border="1px"
+               borderColor="gray.200"
+               marginTop="2"
+            >
+               Search Location
+            </Button>
+            {showLocations && (
+               <Flex flexDir="column" pos="relative" paddingTop="2">
+                  <Input
+                     placeholder="Type Here"
+                     value={searchTerm}
+                     w="300px"
+                     focusBorderColor="gray.300"
+                     onChange={(event) => setSearchTerm(event.target.value)}
+                  />
+                  {searchTerm && (
+                     <Icon
+                        as={MdCancel}
+                        pos="absolute"
+                        cursor="pointer"
+                        right="5"
+                        top="5"
+                        zIndex="100"
+                        onClick={() => setSearchTerm('')}
+                     />
+                  )}
+                  {loading && <Spinner margin="auto" marginTop="3" />}
+                  {showLocations && (
+                     <Box height="300px" overflow="auto">
+                        {locationData?.map(({ id, name }) => (
+                           <Box key={id} onClick={() => handleLocations(name)}>
+                              <Text
+                                 cursor="pointer"
+                                 bg="gray.200"
+                                 p="2"
+                                 borderBottom="1px"
+                                 borderColor="gray.100"
+                              >
+                                 {name}
+                              </Text>
+                           </Box>
+                        ))}
+                        {!loading && !locationData?.length && (
+                           <Flex
+                              justifyContent="center"
+                              alignItems="center"
+                              flexDir="column"
+                              marginTop="5"
+                              marginBottom="5"
+                           >
+                              <Image src={noresult} alt="no result" />
+                              <Text fontSize="xl" marginTop="3">
+                                 Waiting to search!
+                              </Text>
+                           </Flex>
+                        )}
+                     </Box>
+                  )}
+               </Flex>
+            )}
+         </Flex>
       </Flex>
    );
 };
